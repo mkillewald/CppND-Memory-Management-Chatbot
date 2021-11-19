76dbf5cf9c6c5d93b3a1a6307dab6a89dc5904ff
Mike Killewald <mkillewald@yahoo.com>
2021-11-19 11:18:13 -0500

    Task 2 completed

--- src/chatbot.cpp
+++ src/chatbot.cpp
@@ -42,6 +42,72 @@ ChatBot::~ChatBot() {
 //// STUDENT CODE
 ////
 
+ChatBot::ChatBot(const ChatBot &source) {
+  std::cout << "ChatBot Copy Constructor" << std::endl;
+
+  _image = new wxBitmap(*source._image);
+  _currentNode = source._currentNode;
+  _rootNode = source._rootNode;
+  _chatLogic = source._chatLogic;
+}
+
+ChatBot &ChatBot::operator=(const ChatBot &source) {
+  std::cout << "ChatBot Copy Assignment Operator" << std::endl;
+
+  if (this == &source) {
+    return *this;
+  }
+
+  if (_image != NULL) {
+    delete _image;
+  }
+
+  _image = new wxBitmap(*source._image);
+  _currentNode = source._currentNode;
+  _rootNode = source._rootNode;
+  _chatLogic = source._chatLogic;
+
+  return *this;
+}
+
+ChatBot::ChatBot(ChatBot &&source) {
+  std::cout << "ChatBot Move Constructor" << std::endl;
+
+  _image = source._image;
+  _currentNode = source._currentNode;
+  _rootNode = source._rootNode;
+  _chatLogic = source._chatLogic;
+
+  source._image = NULL;
+  source._currentNode = nullptr;
+  source._rootNode = nullptr;
+  source._chatLogic = nullptr;
+}
+
+ChatBot &ChatBot::operator=(ChatBot &&source) {
+  std::cout << "ChatBot Move Assignment Operator" << std::endl;
+
+  if (this == &source) {
+    return *this;
+  }
+
+  if (_image != NULL) {
+    delete _image;
+  }
+
+  _image = source._image;
+  _currentNode = source._currentNode;
+  _rootNode = source._rootNode;
+  _chatLogic = source._chatLogic;
+
+  source._image = NULL;
+  source._currentNode = nullptr;
+  source._rootNode = nullptr;
+  source._chatLogic = nullptr;
+
+  return *this;
+}
+
 ////
 //// EOF STUDENT CODE
 
